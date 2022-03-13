ae142f4965fdc6af25b84a6dd3f6dd1a264d9518
harithaAdika <harithaagr@gmail.com>
2022-03-13 16:46:36 +0530

    Adding the changes to repo

---
+++ src/com/greatlearning/emailapplication/model/Employee.java
@@ -0,0 +1,17 @@
+package com.greatlearning.emailapplication.model;
+
+public class Employee {
+	private String FirstName;
+	private String LastName;
+	public Employee(String FirstName, String LastName) {
+		this.FirstName = FirstName;
+		this.LastName = LastName;
+	}
+	public String getFirstName() {
+		return FirstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+
+}
